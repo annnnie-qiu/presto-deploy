d7e092a3d21c11d0b8cf0ff069685cc276b46ee5
Siyu Qiu <z5348946@student.unsw.edu.au>
2024-11-02 11:36:52 +1100

    finish the welcome page jump to login and reg - 2.1.0

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 
-import LoginBtn from "../components/login/LoginBtn";
-
 function LoginPage() {
   return (
     <div>
       <h1>Login</h1>
-      <LoginBtn />
     </div>
   );
 }
