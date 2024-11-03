f605c50edcb44e8d7bee2cc48cacd9b2fe4ea70d
Junyao <z5324082@ad.unsw.edu.au>
2024-11-03 23:53:20 +1100

    Packages added up

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Flex, Menu } from 'antd';
 import PrestoLogo from "../assets/Presto.png";
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import {
   UserOutlined,
   ProfileOutlined,
@@ -9,12 +11,35 @@ import {
   SettingOutlined, 
 } from '@ant-design/icons';
 
+const StyledMenu = styled(Menu)`
+.ant-menu-item-selected {
+  background-color: #4f6f52 !important;
+  color: #fff !important;
+}
+`;
+
 const Sidebar = () => {
+
   const styles = {
     logo: {
       height: '3vw',
       marginTop: '10px',
+      padding: '15px'
+    },
+    menubar: {
+      marginTop: '20px',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '10px',
+      fontWeight: '500',
     }
+  };
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/');
   };
 
   return (
@@ -26,10 +51,10 @@ const Sidebar = () => {
         </div>
       </Flex>
 
-      <Menu 
+      <StyledMenu 
         mode="inline" 
         defaultSelectedKeys={['1']} 
-        className="menu-bar" 
+        style={styles.menubar} 
         items={[
           {
             key: '1',
@@ -55,6 +80,7 @@ const Sidebar = () => {
             key: '5',
             icon: <LogoutOutlined />,
             label: 'Logout',
+            onClick: handleLogout,
           },
         ]}
       />
