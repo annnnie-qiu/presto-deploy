e2401f39e445e9713755050d35e9d2b6ea2205e1
Siyu Qiu <z5348946@student.unsw.edu.au>
2024-11-11 14:33:24 +1100

    can upload image now

--- frontend/src/components/presentationItem/PresentationText.jsx
+++ frontend/src/components/presentationItem/PresentationText.jsx
@@ -1,6 +1,7 @@
 import Draggable from "react-draggable";
 import React, { useState } from "react";
 import { getDetail } from "../../../utils/API/Send_ReceiveDetail/send_receiveDetail";
+import { Rnd } from "react-rnd";
 
 function PresentationText({
   data,
@@ -16,96 +17,63 @@ function PresentationText({
   const [isMoveActive, setIsMoveActive] = useState(false);
 
   return (
-    <Draggable>
-      <div
-        style={{
-          // width: "1000px",
-          // height: "1000px",
-          width: `${data?.textSizeLength}%`,
-          height: `${data?.textSizeWidth}%`,
-          color: data?.textFontColor,
-          fontSize: `${data?.textFontSize}em`,
-          fontFamily: data?.textFontFamily || 'Quicksand, sans-serif',
-          overflow: "hidden",
-          cursor: isMoveActive ? "move" : "default",
-          position: "relative",
-        }}
-        className=" border border-gray-300 "
-        onDoubleClick={async () => {
-          console.log("double clicked");
+    // <Draggable>
+    <div
+      style={{
+        // width: "1000px",
+        // height: "1000px",
+        width: `${data?.textSizeLength}%`,
+        height: `${data?.textSizeWidth}%`,
+        color: data?.textFontColor,
+        fontSize: `${data?.textFontSize}em`,
+        fontFamily: data?.textFontFamily || "Quicksand, sans-serif",
+        overflow: "hidden",
+        cursor: isMoveActive ? "move" : "default",
+        position: "relative",
+      }}
+      className=" border border-gray-300 "
+      onDoubleClick={async () => {
+        console.log("double clicked");
 
-          setTextSizeLength(data.textSizeLength);
-          setTextSizeWidth(data.textSizeWidth);
-          setTextInput(data.textInput);
-          setTextFontSize(data.textFontSize);
-          setTextFontColor(data.textFontColor);
-          setTextFontFamily(data.textFontFamily);
-          setSelectedElementId(data.id);
-          showTextModal();
-        }}
-        onClick={() => {
-          console.log("clicked");
-          setIsMoveActive(!isMoveActive);
-        }}
-      >
-        {data ? (
-          <span>
-            {data.textInput.split("\n").map((line, index) => (
-              <React.Fragment key={index}>
-                {line}
-                <br />
-              </React.Fragment>
-            ))}
-          </span>
-        ) : null}
-
-        {/* Corner Handles */}
-        {isMoveActive && (
-          <>
-            <div
-              style={{
-                position: "absolute",
-                width: "5px",
-                height: "5px",
-                backgroundColor: "black",
-                top: "-2.5px",
-                left: "-2.5px",
-              }}
-            />
-            <div
-              style={{
-                position: "absolute",
-                width: "5px",
-                height: "5px",
-                backgroundColor: "black",
-                top: "-2.5px",
-                right: "-2.5px",
-              }}
-            />
-            <div
-              style={{
-                position: "absolute",
-                width: "5px",
-                height: "5px",
-                backgroundColor: "black",
-                bottom: "-2.5px",
-                left: "-2.5px",
-              }}
-            />
-            <div
-              style={{
-                position: "absolute",
-                width: "5px",
-                height: "5px",
-                backgroundColor: "black",
-                bottom: "-2.5px",
-                right: "-2.5px",
-              }}
-            />
-          </>
-        )}
+        setTextSizeLength(data.textSizeLength);
+        setTextSizeWidth(data.textSizeWidth);
+        setTextInput(data.textInput);
+        setTextFontSize(data.textFontSize);
+        setTextFontColor(data.textFontColor);
+        // setTextFontFamily(data.textFontFamily);
+        setSelectedElementId(data.id);
+        showTextModal();
+      }}
+      onClick={() => {
+        console.log("clicked");
+        setIsMoveActive(!isMoveActive);
+      }}
+    >
+      <div style={{ margin: 0, height: "100%", paddingBottom: "40px" }}>
+        <Rnd
+          default={{
+            x: 150,
+            y: 205,
+            width: 500,
+            height: 190,
+          }}
+          minWidth={500}
+          minHeight={190}
+          bounds="window"
+        >
+          {data ? (
+            <span>
+              {data.textInput.split("\n").map((line, index) => (
+                <React.Fragment key={index}>
+                  {line}
+                  <br />
+                </React.Fragment>
+              ))}
+            </span>
+          ) : null}
+        </Rnd>
       </div>
-    </Draggable>
+    </div>
   );
 }
 
