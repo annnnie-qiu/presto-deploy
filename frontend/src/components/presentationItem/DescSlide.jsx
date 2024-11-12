34942cd954f78a1ef496bf69c5396ce6331fdc2e
Siyu Qiu <z5348946@student.unsw.edu.au>
2024-11-12 13:07:28 +1100

    start 2.4.3 make the whole presentation screen

---
+++ frontend/src/components/presentationItem/DescSlide.jsx
@@ -0,0 +1,131 @@
+import React, { useRef } from "react";
+import PresentationText from "./PresentationText";
+import PresentationImage from "./PresentationImage";
+import PresentationCode from "./PresentationCode";
+import PresentationVideo from "./PresentationVideo";
+
+
+const DescSlide = ({
+  currentSlides,
+  presentationId,
+  selectedSlideId,
+  showTextModal,
+  setTextSizeLength,
+  setTextSizeWidth,
+  setTextInput,
+  setTextFontSize,
+  setTextFontColor,
+  setSelectedElementId,
+  showImageModal,
+  setImageSizeLength,
+  setImageSizeWidth,
+  setImageAlt,
+  setUploadImage,
+  showCodeModal,
+  setCodeBlockSize,
+  setCodeContent,
+  setCodeFontSize,
+  setCodeLanguage,
+  setCurrentSlides,
+  showVideoModal,
+  setVideoUrl,
+  setVideoSizeLength,
+  setVideoSizeWidth,
+  setVideoAutoplay,
+}) => {
+  const boundsRef = useRef(null);
+  return (
+    <div className="flex h-full w-full justify-center items-center">
+      <div
+        className="bg-white h-5/6 w-11/12 rounded-lg border-solid border-2 border-inherit"
+        style={{ position: "relative" }}
+        ref={boundsRef}
+      >
+        {currentSlides?.map((slide) => {
+          if (slide.slideId === selectedSlideId) {
+            return slide.content?.map((element) => {
+              if (element.type === "text") {
+                return (
+                  <PresentationText
+                    showTextModal={showTextModal}
+                    key={element.id}
+                    data={element}
+                    setTextSizeLength={setTextSizeLength}
+                    setTextSizeWidth={setTextSizeWidth}
+                    setTextInput={setTextInput}
+                    setTextFontSize={setTextFontSize}
+                    setTextFontColor={setTextFontColor}
+                    setSelectedElementId={setSelectedElementId}
+                    boundsRef={boundsRef}
+                    currentSlides={currentSlides}
+                    selectedSlideId={selectedSlideId}
+                    setCurrentSlides={setCurrentSlides}
+                    presentationId={presentationId}
+                  />
+                ); // Use a unique key for each element
+              } else if (element.type === "image") {
+                return (
+                  <PresentationImage
+                    showImageModal={showImageModal}
+                    key={element.id}
+                    data={element}
+                    setImageSizeLength={setImageSizeLength}
+                    setImageSizeWidth={setImageSizeWidth}
+                    setImageAlt={setImageAlt}
+                    setSelectedElementId={setSelectedElementId}
+                    setUploadImage={setUploadImage}
+                    boundsRef={boundsRef}
+                    currentSlides={currentSlides}
+                    selectedSlideId={selectedSlideId}
+                    setCurrentSlides={setCurrentSlides}
+                    presentationId={presentationId}
+                  />
+                );
+              } else if (element.type === "code") {
+                return (
+                  <PresentationCode
+                    showCodeModal={showCodeModal}
+                    key={element.id}
+                    data={element}
+                    setCodeBlockSize={setCodeBlockSize}
+                    setCodeContent={setCodeContent}
+                    setCodeFontSize={setCodeFontSize}
+                    setCodeLanguage={setCodeLanguage}
+                    setSelectedElementId={setSelectedElementId}
+                    boundsRef={boundsRef}
+                    currentSlides={currentSlides}
+                    selectedSlideId={selectedSlideId}
+                    setCurrentSlides={setCurrentSlides}
+                    presentationId={presentationId}
+                  />
+                );
+              } else if (element.type === "video") {
+                return (
+                  <PresentationVideo
+                    showVideoModal={showVideoModal}
+                    key={element.id}
+                    data={element}
+                    setSelectedElementId={setSelectedElementId}
+                    setVideoUrl={setVideoUrl}
+                    setVideoSizeLength={setVideoSizeLength}
+                    setVideoSizeWidth={setVideoSizeWidth}
+                    setVideoAutoplay={setVideoAutoplay}
+                    boundsRef={boundsRef}
+                    currentSlides={currentSlides}
+                    selectedSlideId={selectedSlideId}
+                    setCurrentSlides={setCurrentSlides}
+                    presentationId={presentationId}
+                  />
+                );
+              }
+              return null;
+            });
+          }
+          return null;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default DescSlide;
