3048c5ca79bdd86fc88f47b91e8137a0083358ec
Siyu Qiu <z5348946@student.unsw.edu.au>
2024-11-15 13:35:52 +1100

    Upload

--- frontend/utils/API/apiCall.js
+++ frontend/utils/API/apiCall.js
@@ -1,7 +1,14 @@
-export async function apiCall(method, endpoint, body = {}, queryString = '', token = '') {
+export async function apiCall(
+  method,
+  endpoint,
+  body = {},
+  queryString = "",
+  token = ""
+) {
   // Use environment variables for the backend URL
   // const BASE_URL = "http://localhost:5005";
-  const BASE_URL = 'https://z5348946-presto-be-deploy.vercel.app';
+  const BASE_URL = "https://z5348946-presto-be-deploy.vercel.app";
+
   // Set the target address with query string for GET and DELETE
   const targetAddress = `${BASE_URL}/${endpoint}`;
   // method === "GET"
@@ -12,12 +19,12 @@ export async function apiCall(method, endpoint, body = {}, queryString = '', tok
   const fetchOptions = {
     method,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
   };
 
-  if (method !== 'GET') {
+  if (method !== "GET") {
     fetchOptions.body = JSON.stringify(body);
   }
 
@@ -30,7 +37,7 @@ export async function apiCall(method, endpoint, body = {}, queryString = '', tok
     // Parse and return JSON response
     return await response.json();
   } catch (error) {
-    console.error('API call failed:', error);
+    console.error("API call failed:", error);
     throw error; // Rethrow error for further handling in React components
   }
 }
